62f9b2ab9079cdd8ddc505534e247eb3d33ea3fe
mamari90 <130982006+mamari90@users.noreply.github.com>
2023-07-07 11:31:59 +0200

    update path

--- src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
+++ src/main/java/it/pagopa/devops/springbootshowcase/AppController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/app/selc/2-8")
+@RequestMapping("/app")
 public class AppController {
 
     @Value("${MY_APP_COLOR:}")
